fa90a7a0e3817d34d8cf0b61b3d09cdc58dfce59
Personal - Stakshi <personal-stakshi@MacBookAir.lan>
2025-09-07 10:32:04 +0530

    styles: add css for animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -93,10 +93,30 @@ export default {
           from: { height: 'var(--radix-accordion-content-height)' },
           to: { height: '0' },
         },
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+        ' alide-up': {
+          '0%': { transform: 'translateY(20px)', opacity: '0' },
+          '100%': { transform: 'translateY(0)', opacity: '1' },
+        },
+        'dot-pulse': {
+          '0%, 100%': { transform: 'scale(1)', opacity: '0.3' },
+          '50%': { transform: 'scale(1.2)', opacity: '1' },
+        },
+        'scale-in': {
+          '0%': { transform: 'scale(0.8)', opacity: '0' },
+          '100%': { transform: 'scale(1)', opacity: '1' },
+        },
       },
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
+        'fade-in': 'fade-in 0.6s ease-in-out',
+        'alide-up': 'alide-up 0.5s ease-out',
+        'dot-pulse': 'dot-pulse 1.2s infinite ease-in-out',
+        'scale-in': 'scaleIn 0.6s cubic-bezier(0.6, -0.05, 0.01, 0.99)',
       },
     },
   },
